a090d24cd4ae40a57b193336c180111a337ae259
Fernando Henrique Rosa <ferhrosa@gmail.com>
2018-09-30 17:21:27 -0300

    Add user current position to map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -18,17 +18,30 @@ export class MapComponent implements OnInit {
     hasBuildedMap = false;
 
     map: mapboxgl.Map;
-    style = 'mapbox://styles/mapbox/outdoors-v9';
+    style = 'mapbox://styles/mapbox/streets-v9';
     lat: number = null;
     lng: number = null;
 
     // zoom: number;
     // bounds: mapboxgl.LngLatBounds;
 
+    userPositionSource: mapboxgl.GeoJSONSource;
+
     constructor() { }
 
     ngOnInit() {
         if (this.hasRequestedMapPermission) { this.initializeMap(); }
+    }
+
+    getUserPosition = (): GeoJSON.Feature<GeoJSON.Geometry> => {
+        return {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [this.lng, this.lat],
+            },
+            properties: null,
+        };
     }
 
     initializeMap() {
@@ -43,6 +56,8 @@ export class MapComponent implements OnInit {
                     localStorage.setItem('hasRequestedMapPermission', 'true');
 
                     this.buildMap();
+
+                    navigator.geolocation.watchPosition(p => this.updateCurrentPosition(p));
                 },
                 error => {
                     if (error.code === error.PERMISSION_DENIED) {
@@ -54,17 +69,19 @@ export class MapComponent implements OnInit {
             );
         }
     }
+
     buildMap() {
         this.hasBuildedMap = true;
 
         this.map = new mapboxgl.Map({
             container: container,
             style: this.style,
-            zoom: 18,
-            // minZoom: 13,
-            maxZoom: 19,
+            zoom: 17,
+            maxZoom: 18,
             maxBounds: maxBoundaries,
             center: [this.lng, this.lat],
+            logoPosition: 'top-left',
+            // dragPan: false,
         });
 
         this.map.on('load', () => {
@@ -84,16 +101,33 @@ export class MapComponent implements OnInit {
                 },
             });
 
+            this.map.addSource('user-position', {
+                type: 'geojson',
+                data: this.getUserPosition(),
+            });
+
+            this.userPositionSource = <mapboxgl.GeoJSONSource>this.map.getSource('user-position');
+
+            this.map.addLayer({
+                id: 'user',
+                type: 'symbol',
+                source: 'user-position',
+                layout: {
+                    'icon-image': 'rocket-15',
+                },
+            });
+
         });
-
-        // this.map.on('zoom', () => {
-        //     this.zoom = this.map.getZoom();
-        //     this.bounds = this.map.getBounds();
-        // });
-
-        // this.map.on('move', () => {
-        //     this.bounds = this.map.getBounds();
-        // });
     }
 
+    updateCurrentPosition(position: Position) {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+
+        if (this.userPositionSource) {
+            this.userPositionSource.setData(this.getUserPosition());
+        }
+
+        this.map.panTo([this.lng, this.lat]);
+    }
 }
