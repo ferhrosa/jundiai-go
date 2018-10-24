8a46b1182e32bc86a47a8151601b8911c0aba3bb
Fernando H. Rosa <ferhrosa@gmail.com>
2018-10-24 00:05:38 -0300

    Adjust appearance for bottom tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 import * as mapboxgl from 'mapbox-gl';
 import { tokens } from './../environments/tokens';
@@ -10,9 +11,17 @@ import { tokens } from './../environments/tokens';
 })
 export class AppComponent {
 
-  constructor() {
+  currentPage: string;
+
+  constructor(private router: Router) {
     // Initialize the Mapbox access token. It is not being directly set because of errors on TS typing.
     Object.assign(mapboxgl, { accessToken: tokens.mapbox });
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentPage = event.url.replace('/', '');
+      }
+    });
   }
 
 }
